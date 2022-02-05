92d2c063b6d3d66306f3962b7badfe874dc45892
xdongming <97573465+xdongming@users.noreply.github.com>
2022-02-05 21:46:04 +0800

    Update xiaosai.m

--- 校赛/xiaosai.m
+++ 校赛/xiaosai.m
@@ -1,11 +1,11 @@
 clc
 clear
 
-%¶ÁÈ¡Êı¾İ
-data1=xlsread('ÇúÏß1.xlsx');
+%è¯»å–æ•°æ®
+data1=xlsread('æ›²çº¿1.xlsx');
 x1=data1(:,1);
 y1=data1(:,2);
-data2=xlsread('ÇúÏß2.xlsx');
+data2=xlsread('æ›²çº¿2.xlsx');
 x2=data2(:,1);
 y2=data2(:,2);
 x1max=max(x1);
@@ -13,17 +13,17 @@ y1max=max(y1);
 x1min=min(x1);
 y1min=min(y1);
 
-%ÖÆ×÷Íø¸ñ
+%åˆ¶ä½œç½‘æ ¼
 x=[floor(10*x1min)/10:0.1:ceil(10*x1max)/10];
 y=[floor(10*y1min)/10:0.1:ceil(10*y1max)/10];
 [X,Y]=meshgrid(x,y);
-X=X';     %²»ÖªµÀÎªÉ¶×Ô¶¯×ªÖÃÁË
+X=X';     %ä¸çŸ¥é“ä¸ºå•¥è‡ªåŠ¨è½¬ç½®äº†
 Y=Y';
 xlim=size(X,1);
 ylim=size(X,2);
 z=-ones(xlim,ylim);
 for i=1:size(x1)
-    z(round(10*(x1(i)-x1min))+1,round(10*(y1(i)-y1min))+1)=0;   %½«Ô­ÇúÏß¸ß¶ÈÖÃ0£¬ÆäËûµã¸ß¶ÈÈÔÎª-1
+    z(round(10*(x1(i)-x1min))+1,round(10*(y1(i)-y1min))+1)=0;   %å°†åŸæ›²çº¿é«˜åº¦ç½®0ï¼Œå…¶ä»–ç‚¹é«˜åº¦ä»ä¸º-1
 end
 for i=1:size(x2)
     z(round(10*(x2(i)-x1min))+1,round(10*(y2(i)-y1min))+1)=0;
@@ -32,13 +32,17 @@ for i=1:xlim
     for j=1:ylim
         in1=inpolygon(X(i,j),Y(i,j),x1,y1);    
         in2=inpolygon(X(i,j),Y(i,j),x2,y2);
-        if in1==0||in2==1             %ÈôµãÔÚÇúÏß1Ö®Íâ»òÔÚÇúÏß2Ö®ÄÚ£¬ÔòÌø¹ı
+        if in1==0||in2==1             %è‹¥ç‚¹åœ¨æ›²çº¿1ä¹‹å¤–æˆ–åœ¨æ›²çº¿2ä¹‹å†…ï¼Œåˆ™è·³è¿‡
             continue
         end
         d1=sqrt((X(i,j)-x1).^2+(Y(i,j)-y1).^2);
         d2=sqrt((X(i,j)-x2).^2+(Y(i,j)-y2).^2);
-        z(i,j)=min(min(d1),min(d2));         %¼ÆËãµ½Á½¸öÇúÏßµÄ×îĞ¡¾àÀë£¬½«Õâ¸ö¾àÀëÉèÖÃ³É¸ß
+        z(i,j)=min(min(d1),min(d2));         %è®¡ç®—åˆ°ä¸¤ä¸ªæ›²çº¿çš„æœ€å°è·ç¦»ï¼Œå°†è¿™ä¸ªè·ç¦»è®¾ç½®æˆé«˜
     end
 end
 zlim=max(max(z));
-contour(X,Y,z,[0:0.1:floor(10*zlim)/10])        %»æÖÆµÈ¸ßÏß
+figure()
+hold on
+plot(x1,y1,x2,y2)
+contour(X,Y,z,[0.1:0.1:floor(10*zlim)/10])        %ç»˜åˆ¶ç­‰é«˜çº¿
+hold off
